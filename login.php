3aa0b9225cf98caeef4f2e6d57c86260de912c69
Abdelaziz Adel <zezo.adel.mahmoud@gmail.com>
2026-09-07 06:22:13 +0300

    Add login page with form for user authentication

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form-box">
+        <h2>Login</h2>
+        <form action="#" method="POST">
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="password" name="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+        </form>
+        <p>New here? <a href="signup.php">Sign Up</a></p>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
